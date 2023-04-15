73b089d9c517c39c732036e8ea6d24c35d5d6498
soethandar90 <soethandar18@gmail.com>
2023-04-15 16:16:50 -0500

    Update stock-service

--- stock-service/src/main/java/com/service/stock/StockApplication.java
+++ stock-service/src/main/java/com/service/stock/StockApplication.java
@@ -3,16 +3,13 @@ package com.service.stock;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StockApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
 	}
-	@Bean(name = "modelMapper")
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
-
 }
--- stock-service/src/main/java/com/service/stock/controller/StockController.java
+++ stock-service/src/main/java/com/service/stock/controller/StockController.java
@@ -1,45 +1,15 @@
 package com.service.stock.controller;
 
-import com.service.stock.contract.StockDto;
-import com.service.stock.domain.Stock;
-import com.service.stock.service.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("stocks")
 public class StockController {
-    @Autowired
-    private StockService stockService;
-
-    @PostMapping
-    public ResponseEntity<StockDto> addStock(@RequestBody Stock product) {
-        return new ResponseEntity<StockDto>(stockService.addStock(product), HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{productNumber}")
-    public ResponseEntity<StockDto> updateStock(@PathVariable String productNumber, @RequestBody Stock product) {
-        return new ResponseEntity<>(stockService.updateStock(productNumber, product), HttpStatus.OK);
-    }
-
     @GetMapping("/{productNumber}")
-    public ResponseEntity<Optional<StockDto>> getStock(@PathVariable String productNumber) {
-        return new ResponseEntity<>(stockService.getStock(productNumber), HttpStatus.OK);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<StockDto>> getStockList() {
-        return new ResponseEntity<>(stockService.getStockList(), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{productNumber}")
-    public ResponseEntity deleteStock(@PathVariable String productNumber) {
-        stockService.deleteStock(productNumber);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    public String getStock(@PathVariable String productNumber) {
+        return productNumber;
     }
 }
